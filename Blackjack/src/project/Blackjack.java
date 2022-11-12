95923b07787d2f427b6be1aa2f6d0441683d4653
hunterjohnson2022 <johns713@csusm.edu>
2022-11-11 16:08:44 -0800

    working on menu

--- Blackjack/src/project/Blackjack.java
+++ Blackjack/src/project/Blackjack.java
@@ -1,18 +1,33 @@
 package project;
+import java.awt.Color;
+import java.awt.Font;
 import java.util.*;
 import javax.swing.*;
 
 public class Blackjack {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.print("hello");
-		JFrame frame = new JFrame("My First GUI");
-	       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	       frame.setSize(300,300);
+		startMenu();
+	}
+	
+	public static void startMenu() {
+		JFrame menuFrame = new JFrame("Blackjack");
+	       menuFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	       menuFrame.setSize(900,600);
+	       menuFrame.setLocationRelativeTo(null);
+	       menuFrame.setLayout(null);
+	       menuFrame.setResizable(false);
+	       menuFrame.getContentPane().setBackground(new Color(53,101,77));
+	       
+	       JLabel welcomeLabel = new JLabel("Welcome to Blackjack!");
+	       welcomeLabel.setFont(new Font("Serif", Font.BOLD, 25));
+	       welcomeLabel.setForeground(Color.WHITE);
+	       welcomeLabel.setBounds(300, 0, 300, 150);
+	       menuFrame.getContentPane().add(welcomeLabel);
+	       
 	       JButton button = new JButton("Press");
-	       frame.getContentPane().add(button); // Adds Button to content pane of frame
-	       frame.setVisible(true); //tset
+	       //menuFrame.getContentPane().add(button); // Adds Button to content pane of frame
+	       menuFrame.setVisible(true); //bro
 	}
 
 }
