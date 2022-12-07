7e667a2c4ee73afd048fc402dec79b8facb763f5
larryhaskelljr <larryhaskelljr@localhost>
2022-12-06 16:22:37 -0800

    Changed the "How to" section

--- Blackjack/src/project/Blackjack.java
+++ Blackjack/src/project/Blackjack.java
@@ -2,39 +2,47 @@ package project;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Image;
-import java.util.*;
-import javax.swing.*;
-import javax.swing.event.*;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JSlider;
+import javax.swing.WindowConstants;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
 public class Blackjack {
-	
+
 	public static int betAmount;
 
 	public static void main(String[] args) {
 		startMenu();
 	}
-	
+
 	public static void startMenu() {
 		JFrame menuFrame = new JFrame("Blackjack");
-	       menuFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	       menuFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 	       menuFrame.setSize(900,600);
 	       menuFrame.setLocationRelativeTo(null);
 	       menuFrame.setLayout(null);
 	       menuFrame.setResizable(false);
 	       menuFrame.getContentPane().setBackground(new Color(53,101,77));
-	       menuFrame.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
-	       
+	       menuFrame.setDefaultCloseOperation(WindowConstants.HIDE_ON_CLOSE);
+
 	       JLabel welcomeLabel = new JLabel("Welcome to Blackjack!");
 	       welcomeLabel.setFont(new Font("Serif", Font.BOLD, 25));
 	       welcomeLabel.setForeground(Color.WHITE);
 	       welcomeLabel.setBounds(300, 0, 300, 150);
 	       menuFrame.getContentPane().add(welcomeLabel);
-	       
+
 	       JLabel startChips = new JLabel();
 	       startChips.setIcon(new ImageIcon("images/start_chips.png"));
 	       startChips.setBounds(40, 150, 300, 300);
 	       menuFrame.getContentPane().add(startChips);
-	       
+
 	       JButton startButton = new JButton("Play Game");
 	       menuFrame.getContentPane().add(startButton);
 	       startButton.setBounds(400, 150, 300, 100);
@@ -45,97 +53,97 @@ public class Blackjack {
 	                newGameWindow();
 	            }
 	        });
-	       
+
 	       JButton howButton = new JButton("How to Play");
 	       menuFrame.getContentPane().add(howButton);
 	       howButton.setBounds(400, 300, 300, 100);
-	       menuFrame.setVisible(true); 
+	       menuFrame.setVisible(true);
 	       howButton.addActionListener(new java.awt.event.ActionListener() {
 	            @Override
 	            public void actionPerformed(java.awt.event.ActionEvent evt) {
-	                JOptionPane.showMessageDialog(menuFrame, "This is how you play");
+	                JOptionPane.showMessageDialog(menuFrame, "Choose your bet, then deal.\nAfter the initial deal, decide whether to hit or stay.");
 	            }
 	        });
-	       
+
 	}
-	
+
 	public static void newGameWindow() {
 		JFrame gameFrame = new JFrame("Blackjack");
-	       gameFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	       gameFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 	       gameFrame.setSize(900,600);
 	       gameFrame.setLocationRelativeTo(null);
 	       gameFrame.setLayout(null);
 	       gameFrame.setResizable(false);
 	       gameFrame.getContentPane().setBackground(new Color(53,101,77));
-	       gameFrame.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
-	       
+	       gameFrame.setDefaultCloseOperation(WindowConstants.HIDE_ON_CLOSE);
+
 	       Game game = new Game();
-	       
+
 	       //Player Cards ------------------------------------------------------------------------------------------------------------------
 	       JLabel player_card1 = new JLabel();
 	       //player_card1.setIcon(new ImageIcon(new ImageIcon("images/card.png").getImage().getScaledInstance(100, 150, Image.SCALE_DEFAULT)));
 	       player_card1.setBounds(600, 275, 300, 300);
 	       gameFrame.getContentPane().add(player_card1);
-	       
+
 	       JLabel player_card2 = new JLabel();
 	       //player_card2.setIcon(new ImageIcon(new ImageIcon("images/card.png").getImage().getScaledInstance(100, 150, Image.SCALE_DEFAULT)));
 	       player_card2.setBounds(450, 275, 300, 300);
 	       gameFrame.getContentPane().add(player_card2);
-	       
+
 	       JLabel player_card3 = new JLabel();
 	       //player_card3.setIcon(new ImageIcon(new ImageIcon("images/card.png").getImage().getScaledInstance(100, 150, Image.SCALE_DEFAULT)));
 	       player_card3.setBounds(300, 275, 300, 300);
 	       gameFrame.getContentPane().add(player_card3);
-	       
+
 	       JLabel player_card4 = new JLabel();
 	       //player_card4.setIcon(new ImageIcon(new ImageIcon("images/card.png").getImage().getScaledInstance(100, 150, Image.SCALE_DEFAULT)));
 	       player_card4.setBounds(150, 275, 300, 300);
 	       gameFrame.getContentPane().add(player_card4);
-	       
+
 	       JLabel player_card5 = new JLabel();
 	       //player_card5.setIcon(new ImageIcon(new ImageIcon("images/card.png").getImage().getScaledInstance(100, 150, Image.SCALE_DEFAULT)));
 	       player_card5.setBounds(10, 275, 300, 300);
 	       gameFrame.getContentPane().add(player_card5);
-	       
-	       
+
+
 	       //Dealer Cards -----------------------------------------------------------------------------------------------------------------
 	       JLabel dealer_card1 = new JLabel();
 	       //dealer_card1.setIcon(new ImageIcon(new ImageIcon("images/card.png").getImage().getScaledInstance(100, 150, Image.SCALE_DEFAULT)));
 	       dealer_card1.setBounds(600, 0, 300, 300);
 	       gameFrame.getContentPane().add(dealer_card1);
-	       
+
 	       JLabel dealer_card2 = new JLabel();
 	       dealer_card2.setIcon(new ImageIcon(new ImageIcon("images/facedown.png").getImage().getScaledInstance(100, 150, Image.SCALE_DEFAULT)));
 	       dealer_card2.setBounds(450, 0, 300, 300);
 	       gameFrame.getContentPane().add(dealer_card2);
-	       
-	       
+
+
 	       //Labels ------------------------------------------------------------------------------------------------------------------------
 	       JLabel gameOnLabel = new JLabel("Game On!");
 	       gameOnLabel.setFont(new Font("Serif", Font.BOLD, 25));
 	       gameOnLabel.setForeground(Color.WHITE);
 	       gameOnLabel.setBounds(350, -50, 300, 150);
 	       gameFrame.getContentPane().add(gameOnLabel);
-	       
+
 	       JLabel dealerLabel = new JLabel("(Dealer)");
 	       dealerLabel.setFont(new Font("Serif", Font.BOLD, 15));
 	       dealerLabel.setForeground(Color.WHITE);
 	       dealerLabel.setBounds(750, 50, 300, 150);
 	       gameFrame.getContentPane().add(dealerLabel);
-	       
+
 	       JLabel youLabel = new JLabel("(You)");
 	       youLabel.setFont(new Font("Serif", Font.BOLD, 15));
 	       youLabel.setForeground(Color.WHITE);
 	       youLabel.setBounds(750, 350, 300, 150);
 	       gameFrame.getContentPane().add(youLabel);
-	       
+
 	       JLabel betLabel = new JLabel();
 	       betLabel.setFont(new Font("Serif", Font.BOLD, 50));
 	       betLabel.setForeground(Color.WHITE);
 	       betLabel.setBounds(100, 50, 300, 150);
 	       gameFrame.getContentPane().add(betLabel);
-	       
-	       
+
+
 	       //Buttons ---------------------------------------------------------------------------------------------------------------------------
 	       JButton hitButton = new JButton("Hit");
 	       gameFrame.getContentPane().add(hitButton);
@@ -153,8 +161,8 @@ public class Blackjack {
 	            		else if (hitCount == 3)
 	            			player_card5.setIcon(new ImageIcon(new ImageIcon("images/" + game.getPlayerCard(4) + ".png").getImage().getScaledInstance(100, 150, Image.SCALE_DEFAULT)));
 	            	} else {
-	            		int input = JOptionPane.showOptionDialog(null, 
-	            		"You Drew a " + game.getLatestCard() + " and your total score was " + game.getPlayerScore() + "\nYou lost $" + betAmount + " Game Over", 
+	            		int input = JOptionPane.showOptionDialog(null,
+	            		"You Drew a " + game.getLatestCard() + " and your total score was " + game.getPlayerScore() + "\nYou lost $" + betAmount + " Game Over",
 	            		"BUST!", JOptionPane.OK_CANCEL_OPTION, JOptionPane.INFORMATION_MESSAGE, null, null, null);
 
 	            		if(input == JOptionPane.OK_OPTION)
@@ -164,12 +172,12 @@ public class Blackjack {
 	            		} else {
 	            			gameFrame.dispose();
 	            		}
-	            			
+
 	            	}
-	            	
+
 	            }
 	        });
-	       
+
 	       JButton stayButton = new JButton("Stay");
 	       gameFrame.getContentPane().add(stayButton);
 	       stayButton.setBounds(100, 250, 150, 50);
@@ -177,13 +185,13 @@ public class Blackjack {
 	            @Override
 	            public void actionPerformed(java.awt.event.ActionEvent evt) {
 	            	String message = game.winnerSelection();
-	            	
+
 	            	if(message == "The Player wins.\n") {
-	            	
-	            	int input = JOptionPane.showOptionDialog(null, 
-		            		"The Dealer was hiding a(n) " + game.getDealerCard(1) + " and their total score was " + game.getDealerScore() + 
-		            		"\nYour total score was " + game.getPlayerScore() + 
-		            		" " + game.winnerSelection() + "\n" + "You won $" + betAmount * 2, 
+
+	            	int input = JOptionPane.showOptionDialog(null,
+		            		"The Dealer was hiding a(n) " + game.getDealerCard(1) + " and their total score was " + game.getDealerScore() +
+		            		"\nYour total score was " + game.getPlayerScore() +
+		            		" " + game.winnerSelection() + "\n" + "You won $" + betAmount * 2,
 		            		"Game Over!", JOptionPane.OK_CANCEL_OPTION, JOptionPane.INFORMATION_MESSAGE, null, null, null);
 
 		            		if(input == JOptionPane.OK_OPTION)
@@ -194,10 +202,10 @@ public class Blackjack {
 		            			gameFrame.dispose();
 		            		}
 	            	} else {
-	            		int input = JOptionPane.showOptionDialog(null, 
-			            		"The Dealer was hiding a(n) " + game.getDealerCard(1) + " and their total score was " + game.getDealerScore() + 
-			            		"\nYour total score was " + game.getPlayerScore() + 
-			            		" " + game.winnerSelection(), 
+	            		int input = JOptionPane.showOptionDialog(null,
+			            		"The Dealer was hiding a(n) " + game.getDealerCard(1) + " and their total score was " + game.getDealerScore() +
+			            		"\nYour total score was " + game.getPlayerScore() +
+			            		" " + game.winnerSelection(),
 			            		"Game Over!", JOptionPane.OK_CANCEL_OPTION, JOptionPane.INFORMATION_MESSAGE, null, null, null);
 
 			            		if(input == JOptionPane.OK_OPTION)
@@ -210,12 +218,12 @@ public class Blackjack {
 	            	}
 	            }
 	        });
-	       
-	       
-	       gameFrame.setVisible(true);
-	       
 
-	       
+
+	       gameFrame.setVisible(true);
+
+
+
 	       betAmount = 50;
 	       JOptionPane optionPane = new JOptionPane();
 	       JSlider slider = getSlider(optionPane);
@@ -226,14 +234,14 @@ public class Blackjack {
 	       dialog.setVisible(true);
 	       betAmount = (int) optionPane.getInputValue();
 	       betLabel.setText("Bet: " + betAmount);
-	       
+
 	       player_card1.setIcon(new ImageIcon(new ImageIcon("images/" + game.getPlayerCard(0) + ".png").getImage().getScaledInstance(100, 150, Image.SCALE_DEFAULT)));
 	       player_card2.setIcon(new ImageIcon(new ImageIcon("images/" + game.getPlayerCard(1) + ".png").getImage().getScaledInstance(100, 150, Image.SCALE_DEFAULT)));
 	       dealer_card1.setIcon(new ImageIcon(new ImageIcon("images/" + game.getDealerCard(0) + ".png").getImage().getScaledInstance(100, 150, Image.SCALE_DEFAULT)));
 	}
 
-	//utility 
-	
+	//utility
+
 	  static JSlider getSlider(final JOptionPane optionPane) {
 		    JSlider slider = new JSlider();
 		    slider.setMajorTickSpacing(20);
@@ -241,7 +249,8 @@ public class Blackjack {
 		    slider.setPaintLabels(true);
 		    optionPane.setInputValue(50);
 		    ChangeListener changeListener = new ChangeListener() {
-		      public void stateChanged(ChangeEvent changeEvent) {
+		      @Override
+			public void stateChanged(ChangeEvent changeEvent) {
 		        JSlider theSlider = (JSlider) changeEvent.getSource();
 		        if (!theSlider.getValueIsAdjusting()) {
 		          optionPane.setInputValue(theSlider.getValue());
--- Blackjack/src/project/Game.java
+++ Blackjack/src/project/Game.java
@@ -1,32 +1,31 @@
 package project;
 
-import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.Random;
 
 public class Game {
-	
+
 	private int playerScore = 0;
 	private int dealerScore = 0;
-	private ArrayList<String> playerCards = new ArrayList<String>();
-	private ArrayList<String> dealerCards = new ArrayList<String>();
-	
+	private ArrayList<String> playerCards = new ArrayList<>();
+	private ArrayList<String> dealerCards = new ArrayList<>();
 
-	public Game() {      
-		
+
+	public Game() {
+
 		System.out.println("New Game");
 		playerCardsDistribution();
 		dealerCardsDistribution();
-       
+
 	}
-	
+
 	public String drawCard (String visibility) {
 
         String cards[] = {"Ace","2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};
         Random r = new Random();
         int randomIndex = r.nextInt(13);
-        String drawnCard = cards[randomIndex];       
-        return drawnCard;  
+        String drawnCard = cards[randomIndex];
+        return drawnCard;
     }
 
     public static int assignPoints (int score, String card) {
@@ -34,22 +33,22 @@ public class Game {
         int earnedPoints = 0;
         if (card == "Ace") {
 
-        		
+
                 earnedPoints = 11;
-            
+
 
         } else if (card == "10" || card == "Jack" || card == "Queen" || card == "King") {
 
                 earnedPoints = 10;
-                
+
         } else if (card == "Ace" && score >= 11) {
 
                 earnedPoints = 1;
-                
+
         } else {
 
             earnedPoints = Integer.parseInt(card);
-           
+
         }
         return earnedPoints;
     }
@@ -57,26 +56,26 @@ public class Game {
 
     public void playerCardsDistribution() {
 
-        
+
         String playerCard1 = drawCard("faceUp");
         playerCards.add(playerCard1);
         playerScore += assignPoints(playerScore, playerCard1);
         String playerCard2 = drawCard("faceUp");
         playerCards.add(playerCard2);
         playerScore += assignPoints(playerScore, playerCard2);
-        
+
     }
 
     public void dealerCardsDistribution() {
 
-       
+
         String dealerCardFaceUp = drawCard("faceUp");
         dealerCards.add(dealerCardFaceUp);
         dealerScore += assignPoints(dealerScore, dealerCardFaceUp);
         String dealerCardFaceDown = drawCard("faceDown");
         dealerCards.add(dealerCardFaceDown);
         dealerScore += assignPoints(dealerScore, dealerCardFaceDown);
-        
+
     }
 
     public int drawNewCard() {
@@ -84,8 +83,8 @@ public class Game {
         String newCard = drawCard("faceUp");
         playerCards.add(newCard);
         playerScore += assignPoints(playerScore, newCard);
-        
-        return (playerScore > 21)? 0 : 1;    
+
+        return (playerScore > 21)? 0 : 1;
     }
 
 
@@ -102,29 +101,29 @@ public class Game {
 
             return "Draw!";
         }
-        
+
         return "Draw!";
     }
-    
+
     public int getPlayerScore() {
     	return playerScore;
     }
-    
+
     public int getDealerScore() {
     	return dealerScore;
     }
-    
+
     public String getPlayerCard(int index) {
     	return playerCards.get(index);
     }
-    
+
     public String getLatestCard() {
     	return playerCards.get(playerCards.size() - 1);
     }
-    
+
     public String getDealerCard(int index) {
     	return dealerCards.get(index);
     }
-    
-    
+
+
 }
